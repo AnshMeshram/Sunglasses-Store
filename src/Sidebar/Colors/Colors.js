d0fee892303f63419cfd808051f34645837b7184
Avi Kathuria <avikathuria21@gmail.com>
2025-10-25 18:23:29 +0530

    Optimize Color Filter
    #13

--- src/Sidebar/Colors/Colors.js
+++ src/Sidebar/Colors/Colors.js
@@ -4,83 +4,41 @@ import "./Colors.css";
 import Input from "../../components/Input";
 
 function Colors({ handleChange }) {
+  const colors = [
+    { id: "all", value: "", title: "All", color: "" },
+    { id: "gold", value: "gold", title: "Gold Collection", color: "gold" },
+    { id: "black", value: "black", title: "Black Collection", color: "black" },
+    { id: "yellow", value: "yellow", title: "Yellow Vibrance", color: "yellow" },
+    { id: "transparent", value: "transparent", title: "Transparent Elegance", color: "white" },
+    { id: "orange", value: "orange", title: "Orange Burst", color: "orange" },
+    { id: "blue", value: "blue", title: "Blue Horizon", color: "blue" },
+    { id: "red", value: "red", title: "Red Classic", color: "red" },
+    { id: "purple", value: "purple", title: "Purple Dream", color: "purple" },
+    { id: "green", value: "green", title: "Green Punk", color: "green" },
+    { id: "brown", value: "brown", title: "Brown Classic", color: "brown" },
+  ];
+
   return (
     <div>
       <h1 className="color-title">Colors</h1>
-      <label className="sidebar-label-container">
-        <input onChange={handleChange} type="radio" value="" name="color" />
-        <span className="checkmark"></span>All
-      </label>
-      <Input
-        value="gold"
-        title="Gold Collection"
-        name="color"
-        color="gold"
-        handleChange={handleChange}
-      />
-      <Input
-        value="black"
-        title="Black Collection"
-        name="color"
-        color="black"
-        handleChange={handleChange}
-      />
-      <Input
-        value="yellow"
-        title="Yellow Vibrance"
-        name="color"
-        color="yellow"
-        handleChange={handleChange}
-      />
-      <Input
-        value="transparent"
-        title="Transparent Elegance"
-        name="color"
-        color="white"
-        handleChange={handleChange}
-      />
-      <Input
-        value="orange"
-        title="Orange Burst"
-        name="color"
-        color="orange"
-        handleChange={handleChange}
-      />
-      <Input
-        value="blue"
-        title="Blue Horizon"
-        name="color"
-        color="blue"
-        handleChange={handleChange}
-      />
-      <Input
-        value="red"
-        title="Red Classic"
-        name="color"
-        color="red"
-        handleChange={handleChange}
-      />
-      <Input
-        value="purple"
-        title="Purple Dream"
-        name="color"
-        color="purple"
-        handleChange={handleChange}
-      />
-      <Input
-        value="green"
-        title="Green Punk"
-        name="color"
-        color="green"
-        handleChange={handleChange}
-      />
-      <Input
-        value="brown"
-        title="Brown Classic"
-        name="color"
-        color="brown"
-        handleChange={handleChange}
-      />
+      {colors.map((color) => (
+        <div key={color.id}>
+          {color.id === "all" ? (
+            <label className="sidebar-label-container">
+              <input onChange={handleChange} type="radio" value={color.value} name="test" />
+              <span className="checkmark"></span>All
+            </label>
+          ) : (
+            <Input
+              value={color.value}
+              title={color.title}
+              name="test"
+              color={color.color}
+              handleChange={handleChange}
+            />
+          )}
+        </div>
+      ))}
     </div>
   );
 }
