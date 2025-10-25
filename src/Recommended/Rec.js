d759b3999fbca4df06f626ccc3288f748b23cea5
OmMaheshwari653 <ommaheshwari869@gmail.com>
2025-10-25 12:31:53 +0530

    Fix Color Filter #13

--- src/Recommended/Rec.js
+++ src/Recommended/Rec.js
@@ -1,21 +1,32 @@
-import './Rec.css';
-function Rec() {
-  return (<>
-    <div className='rec-container'>
-      <h1 className='recomended-title'>Recomended</h1>
-      <div className='Recomended-flex'>
-        <button className='btns'>All Products</button>
-        <button className='btns'>Ray-Ban</button>
-        <button className='btns'>Chanel</button>
-        <button className='btns'>Oakley</button>
-        <button className='btns'>Gucci</button>
-        <button className='btns'>Dior</button>
+import "./Rec.css";
+function Rec({ handleClick }) {
+  return (
+    <>
+      <div className="rec-container">
+        <h1 className="recomended-title">Recomended</h1>
+        <div className="Recomended-flex">
+          <button className="btns" onClick={handleClick} value="">
+            All Products
+          </button>
+          <button className="btns" onClick={handleClick} value="Ray-Ban">
+            Ray-Ban
+          </button>
+          <button className="btns" onClick={handleClick} value="Chanel">
+            Chanel
+          </button>
+          <button className="btns" onClick={handleClick} value="Oakley">
+            Oakley
+          </button>
+          <button className="btns" onClick={handleClick} value="Gucci">
+            Gucci
+          </button>
+          <button className="btns" onClick={handleClick} value="Dior">
+            Dior
+          </button>
+        </div>
       </div>
-    </div>
-  
-  
-  </>
-  )
+    </>
+  );
 }
 
-export default Rec
+export default Rec;
--- src/Sidebar/Colors/Colors.js
+++ src/Sidebar/Colors/Colors.js
@@ -1,90 +1,86 @@
 // Colors.js
-import React from 'react';
-import './Colors.css';
-import Input from '../../components/Input';
+import React from "react";
+import "./Colors.css";
+import Input from "../../components/Input";
 
-function Colors() {
-  // Function to handle changes
-  const handleChange = (event) => {
-    console.log(event.target.value); // Or implement your logic
-  };
-
+function Colors({ handleChange }) {
   return (
     <div>
-      <h1 className='color-title'>Colors</h1>
-  <label className='sidebar-label-container'>
-   <input 
-   onChange = {handleChange} 
-   type='radio' 
-   value=''
-   name='test2'
-   />
-   <span className='checkmark'></span>All
-  </label>
-      <Input 
-        value='fancy'
-        title='Fancy Collection'
-        name='color'
-        color='gold'
+      <h1 className="color-title">Colors</h1>
+      <label className="sidebar-label-container">
+        <input onChange={handleChange} type="radio" value="" name="color" />
+        <span className="checkmark"></span>All
+      </label>
+      <Input
+        value="gold"
+        title="Gold Collection"
+        name="color"
+        color="gold"
         handleChange={handleChange}
       />
-      <Input 
-        value='black'
-        title='Black Collection'
-        name='color'
-        color='black'
+      <Input
+        value="black"
+        title="Black Collection"
+        name="color"
+        color="black"
         handleChange={handleChange}
       />
-      <Input 
-        value='yellow'
-        title='Yellow Vibrance'
-        name='color'
-        color='yellow'
+      <Input
+        value="yellow"
+        title="Yellow Vibrance"
+        name="color"
+        color="yellow"
         handleChange={handleChange}
       />
-      <Input 
-        value='transparent'
-        title='Transparent Elegance'
-        name='color'
-        color='white'
+      <Input
+        value="transparent"
+        title="Transparent Elegance"
+        name="color"
+        color="white"
         handleChange={handleChange}
       />
-      <Input 
-        value='orange'
-        title='Orange Burst'
-        name='color'
-        color='orange'
+      <Input
+        value="orange"
+        title="Orange Burst"
+        name="color"
+        color="orange"
         handleChange={handleChange}
       />
-      <Input 
-        value='blue'
-        title='Blue Horizon'
-        name='color'
-        color='blue'
+      <Input
+        value="blue"
+        title="Blue Horizon"
+        name="color"
+        color="blue"
         handleChange={handleChange}
       />
-      <Input 
-        value='red'
-        title='Red Classic'
-        name='color'
-        color='red'
+      <Input
+        value="red"
+        title="Red Classic"
+        name="color"
+        color="red"
         handleChange={handleChange}
       />
-      <Input 
-        value='purple'
-        title='Purple Dream'
-        name='color'
-        color='purple'
+      <Input
+        value="purple"
+        title="Purple Dream"
+        name="color"
+        color="purple"
         handleChange={handleChange}
       />
-      <Input 
-        value=''
-        title='Green Punk'
-        name='color'
-        color='green'
+      <Input
+        value="green"
+        title="Green Punk"
+        name="color"
+        color="green"
         handleChange={handleChange}
       />
- 
+      <Input
+        value="brown"
+        title="Brown Classic"
+        name="color"
+        color="brown"
+        handleChange={handleChange}
+      />
     </div>
   );
 }
